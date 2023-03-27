72876d71b9d6d2d82f631f97738006642110201b
Kanajuktus <amirsaduov6@gmail.com>
2023-03-27 15:38:37 +0600

    Get requests for transports

--- web-backend/transports/urls.py
+++ web-backend/transports/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import (
     get_transports_of_company,
     get_record_for_transport,
+    get_transport,
+    get_route,
+    get_stop,
+    get_company_routes,
+    get_routes_stops,
 
     TransportView, edit_transport, delete_transport,
     StopView, edit_stop, delete_stop,
@@ -19,24 +24,29 @@ urlpatterns = [
     path('add-transport/', TransportView.as_view(), name='add-transport'),
     path('edit-transport/', edit_transport, name='edit-transport'),
     path('delete-transport/', delete_transport, name='delete-transport'),
+    path('get-transport/', get_transport, name='get-transport'),
 
     # Stops
     path('add-stop/', StopView.as_view(), name='add-stop'),
     path('edit-stop/', edit_stop, name='edit-stop'),
     path('delete-stop/', delete_stop, name='delete-stop'),
+    path('get-stop/', get_stop, name='get-stop'),
 
     # Routes
     path('add-route/', RouteView.as_view(), name='add-route'),
     path('edit-route/', edit_route, name='edit-route'),
     path('delete-route/', delete_route, name='delete-route'),
+    path('get-route/', get_route, name='get-route'),
 
     # Company routes
     path('add-company-routes/', CompanyRoutesView.as_view(), name='add-company-route'),
     path('edit-company-routes/', edit_company_routes, name='edit-company-route'),
     path('delete-company-routes/', delete_company_routes, name='delete-company-route'),
+    path('get-company-routes/', get_company_routes, name='get-company-routes'),
 
     # Routes' stops
     path('add-routes-stops/', RoutesStopsView.as_view(), name='add-routes-stops'),
     path('edit-routes-stops/', edit_routes_stops, name='edit-routes-stops'),
     path('delete-routes-stops/', delete_routes_stops, name='delete-routes-stops'),
+    path('get-routes-stops/', get_routes_stops, name='get-routes-stops'),
 ]
