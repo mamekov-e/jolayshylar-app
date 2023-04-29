dad30a55b2f7e4e346efbca022d4479496681d6a
Kamajuktus <45822450+Kamajuktus@users.noreply.github.com>
2023-04-29 17:42:17 +0600

    Update transports

--- web-backend/transports/models.py
+++ web-backend/transports/models.py
@@ -2,41 +2,58 @@ from django.db import models
 from accounts.models import City, Company
 # Create your models here.
 
-
-class Transport(models.Model):
-    company = models.ForeignKey('accounts.Company', on_delete=models.CASCADE)
-    route = models.ForeignKey('Route', on_delete=models.CASCADE)
-    total_seats = models.IntegerField(default=0)
-    normal_seats = models.IntegerField(default=0)
-    disabled_seats = models.IntegerField(default=0)
-    transport_number = models.CharField(max_length=255)
-
-
 class Stop(models.Model):
-    city = models.ForeignKey('accounts.City', on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
     stop_name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'stop'
 
 
 class Route(models.Model):
     route_number = models.CharField(max_length=255)
     route_name = models.CharField(max_length=255)
 
+    class Meta:
+        db_table = 'route'
+
 
 class Routes_stops(models.Model):
     order = models.IntegerField(default=0)
-    route = models.ForeignKey('Route', on_delete=models.CASCADE)
-    stop = models.ForeignKey('Stop', on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, default=1)
+    stop = models.ForeignKey(Stop, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        db_table = 'routes_stops'
+
+
+class Transport(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    total_seats = models.IntegerField(default=0)
+    normal_seats = models.IntegerField(default=0)
+    disabled_seats = models.IntegerField(default=0)
+    transport_number = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'transport'
 
 
 class Companies_routes(models.Model):
-    company = models.ForeignKey('accounts.Company', on_delete=models.CASCADE)
-    route = models.ForeignKey('Route', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, default=1)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        db_table = 'companies_routes'
 
 
 class Stop_record(models.Model):
-    transport = models.ForeignKey('Transport', on_delete=models.CASCADE)
-    stop = models.ForeignKey('Stop', on_delete=models.CASCADE)
+    transport = models.ForeignKey(Transport, on_delete=models.CASCADE)
+    stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
     passenger_in = models.IntegerField(default=0)
     passenger_out = models.IntegerField(default=0)
     timestamp = models.TimeField(default=0)
     cycle_amount = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'stop_record'
--- web-backend/transports/views.py
+++ web-backend/transports/views.py
@@ -21,12 +21,12 @@ from .models import (
 )
 from rest_framework.response import Response
 from .serializers import (
-    TransportSerializer,
-    StopSerializer,
-    Stop_recordSerializer,
+    TransportGETSerializer, TransportPOSTSerializer,
+    StopGETSerializer, StopPOSTSerializer,
+    Stop_recordGETSerializer, Stop_recordPOSTSerializer,
     RouteSerializer,
-    Routes_stopsSerializer,
-    Companies_routesSerializer
+    Routes_stopsGETSerializer, Routes_stopsPOSTSerializer,
+    Companies_routesGETSerializer, Companies_routesPOSTSerializer
 )
 # Create your views here.
 
@@ -36,7 +36,7 @@ from .serializers import (
 def get_record_for_transport(request):
     transport_id = request.query_params['id']
     stop_record = Stop_record.objects.filter(transport__exact=transport_id)
-    data = Stop_recordSerializer(stop_record, many=True)
+    data = Stop_recordGETSerializer(stop_record, many=True)
     return Response(data.data)
 
 
@@ -45,16 +45,18 @@ def get_record_for_transport(request):
 def get_transports_of_company(request):
     company_id = request.query_params['id']
     transport = Transport.objects.filter(company__exact=company_id)
-    data = TransportSerializer(transport, many=True)
+    data = TransportGETSerializer(transport, many=True)
     return Response(data.data)
+
 
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
 def get_routes_of_company(request):
     company_id = request.query_params['id']
     route_ids = Companies_routes.objects.filter(company_id__exact=company_id).select_related('route_id')
-    data = Routes_stopsSerializer(route_ids, many=True)
+    data = Routes_stopsGETSerializer(route_ids, many=True)
     return Response(data.data)
+
 
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
@@ -62,16 +64,18 @@ def get_stops_of_company(request):
     company_id = request.query_params['id']
     route_ids = Companies_routes.objects.filter(company_id__exact=company_id).select_related('route_id')
     stops = Stop.objects.filter(routes_stops__route_id__companies_routes__in=route_ids)
-    data = StopSerializer(stops, many=True)
+    data = StopGETSerializer(stops, many=True)
     return Response(data.data)
+
 
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
 def get_transport(request):
     transport_id = request.query_params['id']
     transport = Transport.objects.filter(id__exact=transport_id).first()
-    data = TransportSerializer(transport)
+    data = TransportGETSerializer(transport)
     return Response(data.data)
+
 
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
@@ -81,37 +85,41 @@ def get_route(request):
     data = RouteSerializer(route)
     return Response(data.data)
 
+
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
 def get_stop(request):
     stop_id = request.query_params['id']
     stop = Stop.objects.filter(id__exact=stop_id).first()
-    data = StopSerializer(stop)
+    data = StopGETSerializer(stop)
     return Response(data.data)
+
 
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
 def get_company_routes(request):
     company_routes_id = request.query_params['id']
     company_routes = Companies_routes.objects.filter(id__exact=company_routes_id).first()
-    data = Companies_routesSerializer(company_routes)
+    data = Companies_routesGETSerializer(company_routes)
     return Response(data.data)
+
 
 @api_view([static.HTTPMethod.get])
 @permission_classes([IsAuthenticated])
 def get_routes_stops(request):
     routes_stops_id = request.query_params['id']
     routes_stops = Routes_stops.objects.filter(id__exact=routes_stops_id)
-    data = Routes_stopsSerializer(routes_stops, many=True)
+    data = Routes_stopsGETSerializer(routes_stops, many=True)
     return Response(data.data)
+
 
 @permission_classes([IsAuthenticated])
 class RecordView(APIView):
     def post(self, request):
-        serializer = Stop_recordSerializer(data=request.data)
+        serializer = Stop_recordPOSTSerializer(data=request.data)
         if serializer.is_valid():
             stop_record = serializer.save()
-            serializer = Stop_recordSerializer(stop_record)
+            serializer = Stop_recordPOSTSerializer(stop_record)
             return Response(
                 {"stop_record": serializer.data}
             )
@@ -124,13 +132,14 @@ class RecordView(APIView):
 @permission_classes([IsAuthenticated])
 class TransportView(APIView):
     def post(self, request):
-        serializer = TransportSerializer(data=request.data)
+        serializer = TransportPOSTSerializer(data=request.data)
         if serializer.is_valid():
             transport = serializer.save()
-            serializer = TransportSerializer(transport)
-            return Response(
-                {"transport": serializer.data}
-            )
+            serializer = TransportPOSTSerializer(transport)
+            data = {
+                "transport": serializer.data
+            }
+            return Response(data)
         else:
             data = serializer.errors
             return Response(data, status=400)
@@ -142,9 +151,9 @@ def edit_transport(request):
     transport_id = request.data['id']
     transport = Transport.objects.get(id__exact=transport_id)
     edit_data = request.data
-    edited_transport_serializer = TransportSerializer(data=edit_data)
+    edited_transport_serializer = TransportPOSTSerializer(data=edit_data)
     edited_transport_serializer.is_valid(raise_exception=True)
-    new_data = TransportSerializer.update(transport, edited_transport_serializer.validated_data)
+    new_data = TransportPOSTSerializer.update(transport, edited_transport_serializer.validated_data)
 
     return Response(new_data.data)
 
@@ -162,10 +171,10 @@ def delete_transport(request):
 @permission_classes([IsAuthenticated])
 class StopView(APIView):
     def post(self, request):
-        serializer = StopSerializer(data=request.data)
+        serializer = StopPOSTSerializer(data=request.data)
         if serializer.is_valid():
             stop = serializer.save()
-            serializer = StopSerializer(stop)
+            serializer = StopPOSTSerializer(stop)
             return Response(
                 {"stop": serializer.data}
             )
@@ -180,9 +189,9 @@ def edit_stop(request):
     stop_id = request.data['id']
     stop = Stop.objects.get(id__exact=stop_id)
     edit_data = request.data
-    edited_stop_serializer = StopSerializer(data=edit_data)
+    edited_stop_serializer = StopPOSTSerializer(data=edit_data)
     edited_stop_serializer.is_valid(raise_exception=True)
-    new_data = StopSerializer.update(stop, edited_stop_serializer.validated_data)
+    new_data = StopPOSTSerializer.update(stop, edited_stop_serializer.validated_data)
 
     return Response(new_data.data)
 
@@ -238,10 +247,10 @@ def delete_route(request):
 @permission_classes([IsAuthenticated])
 class CompanyRoutesView(APIView):
     def post(self, request):
-        serializer = Companies_routesSerializer(data=request.data)
+        serializer = Companies_routesPOSTSerializer(data=request.data)
         if serializer.is_valid():
             companies_routes = serializer.save()
-            serializer = Companies_routesSerializer(companies_routes)
+            serializer = Companies_routesPOSTSerializer(companies_routes)
             return Response(
                 {"companies_routes": serializer.data}
             )
@@ -256,9 +265,9 @@ def edit_company_routes(request):
     companies_routes_id = request.data['id']
     companies_routes = Companies_routes.objects.get(id__exact=companies_routes_id)
     edit_data = request.data
-    edited_companies_routes_serializer = Companies_routesSerializer(data=edit_data)
+    edited_companies_routes_serializer = Companies_routesPOSTSerializer(data=edit_data)
     edited_companies_routes_serializer.is_valid(raise_exception=True)
-    new_data = Companies_routesSerializer.update(companies_routes, edited_companies_routes_serializer.validated_data)
+    new_data = Companies_routesPOSTSerializer.update(companies_routes, edited_companies_routes_serializer.validated_data)
 
     return Response(new_data.data)
 
@@ -276,10 +285,10 @@ def delete_company_routes(request):
 @permission_classes([IsAuthenticated])
 class RoutesStopsView(APIView):
     def post(self, request):
-        serializer = Routes_stopsSerializer(data=request.data)
+        serializer = Routes_stopsPOSTSerializer(data=request.data)
         if serializer.is_valid():
             routes_stops = serializer.save()
-            serializer = Routes_stopsSerializer(routes_stops)
+            serializer = Routes_stopsPOSTSerializer(routes_stops)
             return Response(
                 {"routes_stops": serializer.data}
             )
@@ -294,9 +303,9 @@ def edit_routes_stops(request):
     routes_stops_id = request.data['id']
     routes_stops = Companies_routes.objects.get(id__exact=routes_stops_id)
     edit_data = request.data
-    edited_routes_stops_serializer = Routes_stopsSerializer(data=edit_data)
+    edited_routes_stops_serializer = Routes_stopsPOSTSerializer(data=edit_data)
     edited_routes_stops_serializer.is_valid(raise_exception=True)
-    new_data = Routes_stopsSerializer.update(routes_stops, edited_routes_stops_serializer.validated_data)
+    new_data = Routes_stopsPOSTSerializer.update(routes_stops, edited_routes_stops_serializer.validated_data)
 
     return Response(new_data.data)
 
@@ -305,6 +314,6 @@ def edit_routes_stops(request):
 @permission_classes([IsAuthenticated])
 def delete_routes_stops(request):
     routes_stops_id = request.data['id']
-    routes_stops = Routes_stopsSerializer.objects.get(id__exact=routes_stops_id)
+    routes_stops = Routes_stopsPOSTSerializer.objects.get(id__exact=routes_stops_id)
     routes_stops.delete()
     return Response({'success': True})
